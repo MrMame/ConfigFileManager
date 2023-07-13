2a97fb155cd467a772b011371adddbed2f233152
Markus <51000524+MrMame@users.noreply.github.com>
2023-07-13 12:21:23 +0200

    remove todo-marker

---
+++ src/test/java/de/mme/cfm/testing/tests/UniqueConfiguration/Test_UniqueConfiguration_setEntry_Configurationentry.java
@@ -0,0 +1,32 @@
+package de.mme.cfm.testing.tests.UniqueConfiguration;
+
+import de.mme.cfm.ConfigurationEntry;
+import de.mme.cfm.configurations.Configuration;
+import de.mme.cfm.configurations.UniqueConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test_UniqueConfiguration_setEntry_Configurationentry {
+
+    @Test
+    void IfConfigurationEntryIsNull_throwsIllegalArgumentException() {
+
+        //Arrange
+        ConfigurationEntry nullEntry = null;
+
+        Configuration testUnit = new UniqueConfiguration();
+        testUnit.setEntry("MySetting","MyValue");
+        testUnit.setEntry("MySetting2","MyValue2");
+
+        //ACT
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            //Code under test
+            testUnit.setEntry(nullEntry);
+        });
+        // Assert
+        Assertions.assertEquals("Entry can not be null", thrown.getMessage());
+    }
+
+    // todo : Tests implementieren
+
+}
