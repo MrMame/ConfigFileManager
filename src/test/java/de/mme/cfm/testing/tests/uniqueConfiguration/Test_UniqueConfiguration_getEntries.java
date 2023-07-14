5ff0c4c213b6bebd0db43ca2422278202b7dd897
Markus <51000524+MrMame@users.noreply.github.com>
2023-07-14 22:10:30 +0200

    - Adding tests for ensure that getEntry() and getEntries() are returning not original references.

---
+++ src/test/java/de/mme/cfm/testing/tests/uniqueConfiguration/Test_UniqueConfiguration_getEntries.java
@@ -0,0 +1,42 @@
+package de.mme.cfm.testing.tests.uniqueConfiguration;
+
+import de.mme.cfm.configurations.Configuration;
+import de.mme.cfm.configurations.UniqueConfiguration;
+import de.mme.cfm.data.ConfigurationEntry;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test_UniqueConfiguration_getEntries {
+
+    @Test
+    void ReturnedEntries_AreOnlyCopiesOfOriginalEntries() {
+
+        //Arrange
+        String settingNameA = "MySettingA";
+        String settingNameB = "MySettingB";
+        String settingNameC = "MySettingC";
+
+        ConfigurationEntry originalEntryA = new ConfigurationEntry();
+        ConfigurationEntry originalEntryB = new ConfigurationEntry();
+        ConfigurationEntry originalEntryC = new ConfigurationEntry();
+        originalEntryA.setName(settingNameA).setValue("MyValueA");
+        originalEntryB.setName(settingNameA).setValue("MyValueB");
+        originalEntryC.setName(settingNameA).setValue("MyValueC");
+
+        Configuration testUnit = new UniqueConfiguration();
+        testUnit
+                .setEntry(originalEntryA)
+                .setEntry(originalEntryB)
+                .setEntry(originalEntryC);
+
+        //ACT
+        boolean areSameConfigurationObjects
+                = ((testUnit.getEntry(settingNameA)==originalEntryA)
+                && (testUnit.getEntry(settingNameB)==originalEntryB)
+                && (testUnit.getEntry(settingNameC)==originalEntryC));
+
+        // Assert
+        Assertions.assertEquals(false, areSameConfigurationObjects);
+    }
+
+}
