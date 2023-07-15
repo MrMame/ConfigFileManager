f4d5b483a4ed3bf0f9e5f2c9801f9071bde05acf
Markus <51000524+MrMame@users.noreply.github.com>
2023-07-15 15:31:33 +0200

    - Create class

---
+++ src/main/java/de/mme/cfm/data/ConfigurationEntries.java
@@ -0,0 +1,34 @@
+package de.mme.cfm.data;
+
+
+public class ConfigurationEntries {
+
+    /**
+     * Returns an ConfigurationEntry with the specified present non-null name and non-null value.
+     * @param name - name of the configuration
+     * @param value - value of the configuration
+     * @return A ConfigurationEntry with name-value pair
+     */
+    public static ConfigurationEntry of(String name, String value){
+        if(name==null || value==null)throw new IllegalArgumentException("Name or value may not be NULL!");
+        return new ConfigurationEntry().setName(name).setValue(value);
+    }
+
+    /**
+     * Returns a deep cloned ConfigurationEntry of present entry.
+     * @param entry - Entry to clone. May not be null
+     * @return A Cloned ConfigurationEntry Object of entry
+     */
+    public static ConfigurationEntry deepClone(ConfigurationEntry entry){
+        if(entry==null )throw new IllegalArgumentException("Entry may not be NULL!");
+
+        ConfigurationEntry retCe = new ConfigurationEntry();
+        retCe
+                .setName(entry.getName())
+                .setValue(entry.getValue());
+
+        return retCe;
+    }
+
+
+}
