c6401890e9420163ea282fa8e11a02ae404651ab
Markus <51000524+MrMame@users.noreply.github.com>
2023-07-15 15:32:06 +0200

    - overwrite .hasCode() and .equals() Methods

--- src/main/java/de/mme/cfm/data/ConfigurationEntry.java
+++ src/main/java/de/mme/cfm/data/ConfigurationEntry.java
@@ -1,6 +1,7 @@
 package de.mme.cfm.data;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ConfigurationEntry implements Serializable{
 
@@ -14,4 +15,17 @@ public class ConfigurationEntry implements Serializable{
 
     public ConfigurationEntry setName(String name) {this.name = name;return this;}
     public ConfigurationEntry setValue(String value) {this.value = value;return this;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConfigurationEntry that = (ConfigurationEntry) o;
+        return Objects.equals(name, that.name) && Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, value);
+    }
 }
